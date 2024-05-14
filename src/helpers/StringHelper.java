3706a3e94a7a0cb38fc76fcbfe2e813b99f65402
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-05-14 08:20:31 -0300

    change string helper

--- src/helpers/StringHelper.java
+++ src/helpers/StringHelper.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Letter;
+import entities.stateEnum;
 
 public class StringHelper {
-    
-    
+
     public static List<Letter> setStringToLetter(String word) {
 
         char[] array = word.toCharArray();
@@ -21,19 +21,30 @@ public class StringHelper {
         return letterList;
     }
 
-    public static void isInAnswer(Letter letter, String answer){
-        
-        char[] separated_answer = new char[answer.length];
+    public static boolean isInAnswer(Letter letter, String answer, int letterIndex) {
 
-        for (int i = 0; i<answer.length; i++){
+        char[] separated_answer = new char[answer.length()];
+
+        for (int i = 0; i < answer.length(); i++) {
             separated_answer[i] = answer.charAt(i);
         }
 
-        for(int i = 0; i<answer.length; i++){
-            if (letter.getLetter() == separated_answer[i]){
-                letter.setState(DISCOVERED_AND_WRONG);
+        boolean isWrong = true;
+        for (int i = 0; i < answer.length(); i++) {
+            if (letter.getLetter() == separated_answer[i]) {
+                isWrong = false;
+                letter.setState(stateEnum.DISCOVERED_AND_WRONG);
+                if (i == letterIndex) {
+                    letter.setState(stateEnum.DISCOVERED_AND_RIGHT);
+                    return true;
+                }
             }
         }
+
+        if (isWrong) {
+            letter.setState(stateEnum.WRONG);
+        }
+        return false;
 
     }
 }
