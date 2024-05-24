984564624c9824c9020446d333b0520b5ce946d2
Breno Amorim <102449371+AmorimBreno@users.noreply.github.com>
2024-05-24 12:06:25 -0300

    menu tela

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,14 +4,20 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
+import ui.Menu.MenuScreen;
+
 public class Main extends JFrame {
 
     private GameScreen gameScreen;
+    private MenuScreen menu;
 
     public Main() {
-        gameScreen = new GameScreen();
-        gameScreen.setBackground(Color.DARK_GRAY);
-        add(gameScreen);
+        // gameScreen = new GameScreen();
+        // gameScreen.setBackground(Color.DARK_GRAY);
+        // add(gameScreen);
+
+        menu = new MenuScreen();
+        add(menu);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         setVisible(true);
